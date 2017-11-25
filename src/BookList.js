6097ae59ca74c85835f779d6c80b4fcc2ff461a1
Scott Baker <sbaker1505@gmail.com>
2017-11-25 13:54:44 -0800

    Moved Book components from BookList.js to Book.js

--- src/BookList.js
+++ src/BookList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class BookList extends React.Component {
   bookshelf = [
@@ -30,46 +31,12 @@ class BookList extends React.Component {
               <div key={bookshelf.id}>
                 <h2 className="bookshelf-title">{bookshelf.title}</h2>
                 <div className="bookshelf-books">
-                  <ol className="books-grid">
-                    {/* Start of books */}
-                    {this.props.books
-                      .filter((book) => bookshelf.id === book.shelf)
-                      .map((book) => (
-                      <li key={book.id}>
-                        <div className="book">
-                          <div className="book-top">
-                            <div className="book-cover"
-                              style={{
-                                width: 128,
-                                height: 193,
-                                backgroundImage: `url(${book.imageLinks.thumbnail})`
-                              }}></div>
-                            {/* Bookshelf changer dropdown menu */}
-                            <div className="book-shelf-changer">
-                              <select value={bookshelf.id} readOnly onChange={(event) => this.props.onUpdateShelf(book, event.target.value)}>
-                                <optgroup label="Move to...">
-                                  {this.bookshelf.map((bookshelf) => (
-                                    <option
-                                      key={bookshelf.id}
-                                      value={bookshelf.id}
-                                      >
-                                      {bookshelf.title}
-                                    </option>
-                                  ))}
-                                </optgroup>
-                              </select>
-                            </div>
-                          </div>
-                          <div className="book-title">{book.title}</div>
-                          <div className="book-authors">
-                            {book.authors.map((author) => (
-                              <p key={author.toString().replace(/[.,\s]/g,'')}>{author}</p>
-                            ))}
-                          </div>
-                        </div>
-                      </li>
-                    ))}
-                  </ol>
+                  <Book
+                    bookshelfList={this.bookshelf}
+                    bookshelf={bookshelf}
+                    books={this.props.books}
+                    onUpdateShelf={this.props.onUpdateShelf}
+                  />
                 </div>
               </div>
             ))}
