0fd4e9989195cc69cc1640ee2afae58f3fa53df2
Scott Baker <sbaker1505@gmail.com>
2017-11-24 22:21:59 -0800

    Combined Bookshelf.js and Book.js files into BookList.js and deleted files

--- src/BookList.js
+++ src/BookList.js
@@ -1,8 +1,22 @@
 import React from 'react'
-import Bookshelf from './Bookshelf'
 import { Link } from 'react-router-dom'
 
 class BookList extends React.Component {
+  bookshelf = [
+    {
+      title: 'Currently Reading',
+      id: 'currentlyReading'
+    },
+    {
+      title: 'Want to Read',
+      id: 'wantToRead'
+    },
+    {
+      title: 'Read',
+      id: 'read'
+    }
+  ]
+
   render() {
     return (
       <div className="list-books">
@@ -10,7 +24,37 @@ class BookList extends React.Component {
           <h1>MyReads</h1>
         </div>
         <div className="list-books-content">
-          <Bookshelf />
+          <div className="bookshelf">
+            {this.bookshelf.map((bookshelf) => (
+              <div key={bookshelf.id}>
+                <h2 className="bookshelf-title">{bookshelf.title}</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    {this.props.books.filter((books) => bookshelf.id === books.shelf).map((book) => (
+                      <li key={book.id}>
+                        <div className="book">
+                          <div className="book-top">
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-shelf-changer">
+                              <select>
+                                <option value="none" disabled>Move to...</option>
+                                <option value="currentlyReading">Currently Reading</option>
+                                <option value="wantToRead">Want to Read</option>
+                                <option value="read">Read</option>
+                                <option value="none">None</option>
+                              </select>
+                            </div>
+                          </div>
+                          <div className="book-title">{book.title}</div>
+                          <div className="book-authors">{book.authors[0]}</div>
+                        </div>
+                      </li>
+                    ))}
+                  </ol>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
         <div className="open-search">
           <Link
